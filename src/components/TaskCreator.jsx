5665f094b7b07ae9221944d1a6f6a8243eddfe55
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-08-04 14:56:17 -0600

    list tasks

--- src/components/TaskCreator.jsx
+++ src/components/TaskCreator.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 
-const TaskCreator = () => {
+const TaskCreator = ({createNewTask}) => {
 
-const [newTaskName, setNewTaskName] = useState();
+
+
+const [newTaskName, setNewTaskName] = useState('');
 
 const handleSubmit = (e) =>{
     {/* It is used to not send the form data to a backend and the page is not refreshed.*/}
     e.preventDefault();
     {/* It is used to store data within the browser and so we close the application or change the page or exit the application, the data will continue to be stored here.*/}
+    createNewTask(newTaskName)
     localStorage.setItem('tasks', newTaskName)
     setNewTaskName('')
 }
@@ -18,7 +21,7 @@ const handleSubmit = (e) =>{
   return (
       <form onSubmit={handleSubmit}>
       {/* It was added in a form so that when you hit enter in the input you can perform some action. */}
-      
+
                                                           {/* The ONCHANGE={} event is used to receive or capture what the user adds to the input. */}
         <input
         type="text"
