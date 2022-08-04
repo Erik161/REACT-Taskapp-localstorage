bd0bc6588a545ce71cbfbe93b239a34681b04078
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-08-03 21:17:08 -0600

    add create task component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import TaskCreator from "./components/TaskCreator";
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+
 
   return (
     <div className="App">
-      
+      <TaskCreator/>
     </div>
-  )
+  );
 }
 
-export default App
+
+
+export default App;
---
+++ src/components/TaskCreator.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const TaskCreator = () => {
+
+const [newTaskName, setNewTaskName] = useState();
+
+const handleSubmit = (e) =>{
+    {/* It is used to not send the form data to a backend and the page is not refreshed.*/}
+    e.preventDefault();
+    {/* It is used to store data within the browser and so we close the application or change the page or exit the application, the data will continue to be stored here.*/}
+    localStorage.setItem('tasks', newTaskName)
+    setNewTaskName('')
+}
+
+
+
+
+  return (
+      <form onSubmit={handleSubmit}>
+      {/* It was added in a form so that when you hit enter in the input you can perform some action. */}
+      
+                                                          {/* The ONCHANGE={} event is used to receive or capture what the user adds to the input. */}
+        <input
+        type="text"
+        placeholder="Enter a new Task"
+        value={newTaskName}
+        onChange={(e)=> setNewTaskName(e.target.value)}/>
+        <button> Save Task</button>
+      </form>
+  )
+}
+
+export default TaskCreator
