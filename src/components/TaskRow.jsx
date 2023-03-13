3d278a0d54f51d407be84610e98438ed9413395d
Erik Hernandez <erikestuardo_161@hotmail.com>
2023-03-12 18:38:41 -0600

    Modification so that when the tasks are done they are underlined

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const TaskRow = ({task,toggleTask}) => {
+const TaskRow = ({task, toggleTask}) => {
+  const rowClassName = task.done ? 'completed' : '';
   return (
-    <tr>
+    <tr className={rowClassName}>
         <td className='d-flex justify-content-between'>
             {task.name}
             <input type="checkbox" 
             checked={task.done}
-            onChange={()=>toggleTask(task)}
+            onChange={() => toggleTask(task)}
             />
         </td>
     </tr>
